a70f940f8358641bdbbfe5ee4705d8902e3a60c6
Michael Eboagu <Michael Eboagu>
2017-05-10 11:41:48 +0100

    all tests passed

--- app/car.js
+++ app/car.js
@@ -1,19 +1,48 @@
+
 class Car{
-  constructor(name,model){
+  constructor(name,model,type){
     this.name = name ;
     this.model = model;
     this.numOfDoors = 4;
+    this.type = type;
+    this.numOfWheels =  4;
+    this.isSaloon = true;
+    this.speed = 0 + " km/h";
     
     if(this.name === "Porshe" || this.name === "Koenigsegg"){
       this.numOfDoors = 2;
-      this.speed = '250 km/h';
+      //this.speed = '250 km/h';
     }
 
     if(this.name === undefined || this.model === undefined){
       this.name = "General";
       this.model = "GM";
     }
+
+    if(this.type === "trailer"){
+      this.numOfWheels = 8;
+      this.isSaloon = false;
+    }
+
   }
+
+   drive(time){
+    if(this.name == "Porshe"){
+      let distance = 1250;
+      this.speed = (distance/time) + " km/h" ;
+      return this.speed ;
+    }
+
+    if(this.type === "trailer"){
+      let distance = 539;
+      this.speed = (distance/time) + " km/h";
+      return this;
+    }
+
+
+
+  }
+
 }
 
 module.exports = Car;
